4116ae48feb537211a460a1a0e3df3b80a6b89e6
NoelStafford <noeljordan34@gmail.com>
2022-12-07 18:10:26 -0600

    added the indexjs

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment')
+// connect the routes
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { Post, Comment, User}
