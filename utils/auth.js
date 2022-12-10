e933c8a207b9c01b4c2137824e357596a3980ef1
NoelStafford <noeljordan34@gmail.com>
2022-12-10 12:29:13 -0600

    added the auth helper

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// if the user is not logged in will redirect them to the log in page
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
